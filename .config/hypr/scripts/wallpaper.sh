c685ed80d0c1c1be8d3826c71308b6cd0354f1e8
mattis <mattis.backhaus@maxe-del.de>
2026-01-10 14:54:39 +0100

    update hyprland, kitty, waybar, spicetify confs
    Signed-off-by: mattis <mattis.backhaus@maxe-del.de>

---
+++ .config/hypr/scripts/wallpaper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hyprctl hyprpaper preload ~/Wallpapers/The-Great-Wave-of-Kanagawa-Black-and-White2.jpg
+hyprctl hyprpaper wallpaper ,~/Wallpapers/The-Great-Wave-of-Kanagawa-Black-and-White2.jpg
