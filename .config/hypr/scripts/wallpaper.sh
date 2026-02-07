4fe1235af23192c5e331fbc57466060b8c239a9e
mattis <mattis.backhaus@maxe-del.de>
2026-02-07 22:07:04 +0100

    fixed monitor naming and hyprpaper config
    Signed-off-by: mattis <mattis.backhaus@maxe-del.de>

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-hyprctl hyprpaper preload ~/Wallpapers/evangelion.jpg
-hyprctl hyprpaper wallpaper ,~/Wallpapers/evangelion.jpg
+hyprctl hyprpaper preload "~/Wallpapers/evangelion.jpg"
+hyprctl hyprpaper wallpaper "DP-2,~/Wallpapers/evangelion.jpg"
+hyprctl hyprpaper wallpaper "HDMI-A-2,~/Wallpapers/evangelion.jpg"
